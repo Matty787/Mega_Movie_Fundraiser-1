88f20b104549d37a6e72cddbb1fda5fb1103a8ce
Matthew McLean <90365647+Matty787@users.noreply.github.com>
2022-05-25 23:38:56 +0000

    I added the not_blank response

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,25 @@
+#import statements
+
+
+#functions to here
+
+
+# ********** Main Routine **********
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask user if they have used the program before & show instructions if necessary
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+    # Get age (between 12 and 130)
+    # Calculate ticket price
+    # Loop to ask for snacks
+    # Calculate snack price
+
+    #ask for payment method (and apply surcharge if necessary)
+
+#Calculate Total sales and profit
+
+#Output data to text file
